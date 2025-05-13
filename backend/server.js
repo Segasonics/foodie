dc498ac65d06b94209b626734eb26efd96226cbf
Segasonics <114384404+Segasonics@users.noreply.github.com>
2025-05-14 04:18:24 +0530

    folder change

--- backend/server.js
+++ backend/server.js
@@ -36,11 +36,11 @@ app.all('/api/{*splat}', (req, res) => {
 });
 // Serve frontend in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
 }
 
 app.get(/^\/(?!api\/).*/, (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+      res.sendFile(path.resolve(__dirname, '..', 'frontend', 'dist', 'index.html'));
   });
 
 app.listen(PORT,()=>{
